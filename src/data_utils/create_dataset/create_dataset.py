a1979fe1356f933dfdd0195ea3ddd9a68ed1620f
prashant905 <prashant905@gmail.com>
2020-11-23 15:32:45 +0000

    FIX: fixing the depthmap channels issue causing the problem with evaluation

--- src/data_utils/create_dataset/create_dataset.py
+++ src/data_utils/create_dataset/create_dataset.py
@@ -1,22 +1,23 @@
-import pathlib
-import utils
-import shutil
-from data_utils import CollectQrcodes
-import multiprocessing
-import os
-import pickle
-import logging
-import yaml
 import numpy as np
-import pcd2depth
+import yaml
+import logging
+import pickle
+import os
+import multiprocessing
+import shutil
+from src.common.depthmap_toolkit import utils
 import sys
+from pathlib import Path
 sys.path.append('.')
-sys.path.append('../../common/depthmap_toolkit/')
+sys.path.append('../../common/depthmap_toolkit')
+sys.path.append(str(Path(__file__).parents[1]))
+# import pandas as pd
+from data_utils import CollectQrcodes  # noqa: E402
+from src.common.depthmap_toolkit import pcd2depth  # noqa: E402
 
 # Load the yaml file
-with open("parameters.yml", "r") as ymlfile:
+with open("src/data_utils/create_dataset/parameters.yml", "r") as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-
 
 # Parse all the configuration variables
 
@@ -50,9 +51,9 @@ new_scangroup_data = dataset.get_usable_data(dataframe=scangroup_qrcodes, amount
 # 'delete']  #TODO: check for delete tag presence and remove those rows
 
 full_dataset = dataset.merge_qrcode_dataset(new_scangroup_data, scangroup_data)
-
 logging.info("Saving the csv file for EDA notebook.")
 full_dataset.to_csv('evaluation.csv', index=False)
+# full_dataset = pd.read_csv('src/data_utils/create_dataset/evaluation.csv')
 
 # Create the RGB csv file for posenet.
 
@@ -63,14 +64,12 @@ get_rgb_qrcodedata = dataset.merge_data_artifacts(full_dataset, get_rgb_artifact
 get_posenet_results = dataset.merge_data_posenet(full_dataset, get_posenet_data)
 
 get_posenet_results.to_csv("RGB_poseresults.csv", index=False)
+# get_posenet_results = pd.read_csv('src/data_utils/create_dataset/RGB_pose1.csv')
 
 #Read the Calibration file and set the required shape fro height and width
-calibration = utils.parseCalibration(calibration_file)
+# calibration = utils.parseCalibration(calibration_file)
 Width = utils.setWidth(int(240 * 0.75))
 Height = utils.setHeight(int(180 * 0.75))
-
-
-logging.warning("Processing the data:")
 
 
 def process_data(rows):
@@ -78,24 +77,26 @@ def process_data(rows):
     qrcode = rows['qrcode']
     # qrcode = rows['storage_path'].split('/')[1]
     pcdfile = rows['storage_path'].split('/')[-1]
-    depthmaps = pcd2depth.process(calibration, source_path)
+    depthvalues = pcd2depth.process(calibration_file, source_path)
+    depthmaps = depthvalues[:, :, 2]
+    depthmaps = np.expand_dims(depthmaps, axis=2)
     max_value = depthmaps.max()
     if max_value > 10:
         logging.warning(pcdfile)
         return
     scantype = pcdfile.split('_')[3]
     pickle_file = pcdfile.replace('.pcd', '.p')
-    labels = np.array([float(rows['height']), float(rows['weight']), float(
-        rows['muac']), rows['age'], rows['sex'], rows['tag'], rows['scan_group']])
+    labels = np.array([rows['height'], rows['weight'],
+                       rows['muac'], rows['age'], rows['sex'], rows['tag'], rows['scan_group']])
     depthmap_target_path = os.path.join(depthmap_path, qrcode)
     depthmap_complete_path = os.path.join(depthmap_target_path, scantype)
-    pathlib.Path(depthmap_complete_path).mkdir(parents=True, exist_ok=True)
+    Path(depthmap_complete_path).mkdir(parents=True, exist_ok=True)
     data = (depthmaps, labels)
     depthmap_save_path = depthmap_complete_path + '/' + pickle_file
     pickle.dump(data, open(depthmap_save_path, "wb"))
     pcd_target_path = os.path.join(pcd_path, qrcode)
     pcd_complete_path = os.path.join(pcd_target_path, scantype)
-    pathlib.Path(pcd_complete_path).mkdir(parents=True, exist_ok=True)
+    Path(pcd_complete_path).mkdir(parents=True, exist_ok=True)
     shutil.copy(source_path, pcd_complete_path)
     return
 
@@ -108,7 +109,7 @@ def process_RGB(rows):
     scantype = imagefile.split('_')[3]
     rgb_target_path = os.path.join(rgb_path, qrcode)
     rgb_complete_path = os.path.join(rgb_target_path, scantype)
-    pathlib.Path(rgb_complete_path).mkdir(parents=True, exist_ok=True)
+    Path(rgb_complete_path).mkdir(parents=True, exist_ok=True)
     shutil.copy(source_path, rgb_complete_path)
     return
 
@@ -117,16 +118,16 @@ proc = multiprocessing.Pool()
 
 for index, row in full_dataset.iterrows():
     # process_data(row)
-    proc.apply_async(process_data, [row])
+    proc.apply_async(process_data, [row])  # Activate multiptrocessing
 
 proc.close()
 proc.join()  # Wait for all child processes to close.
 
 proc = multiprocessing.Pool()
 
-for index, row in full_dataset.iterrows():
+for index, row in get_posenet_results.iterrows():
     # process_RGB(row)
-    proc.apply_async(process_RGB, [row])
+    proc.apply_async(process_RGB, [row])  # Activate multiprocessing
 
 proc.close()
 proc.join()
