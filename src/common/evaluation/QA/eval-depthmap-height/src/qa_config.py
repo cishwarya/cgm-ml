e9ea2047933f92a15e9fc3a15dfe2bc95e8820a1
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2020-11-27 17:26:11 +0100

    Create flag for stdtest, Make flake8 happy

--- src/common/evaluation/QA/eval-depthmap-height/src/qa_config.py
+++ src/common/evaluation/QA/eval-depthmap-height/src/qa_config.py
@@ -4,8 +4,8 @@ USE_WEIGHT = False
 assert USE_HEIGHT != USE_WEIGHT, "Please enable either HEIGHT or WEIGHT"
 
 if USE_HEIGHT:
-    from .qa_config_height import *
+    from .qa_config_height import *  # noqa: F401, F403
 elif USE_WEIGHT:
-    from .qa_config_weight import *
+    from .qa_config_weight import *  # noqa: F401, F403
 else:
     raise Exception("Please choose either weight or height!")
